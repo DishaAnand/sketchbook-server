39b09bddad716aa0e92a0a1c0372002461843ec2
DISHA ANAND <ds442718@dal.ca>
2024-01-04 12:34:06 -0400

    feat:emitting events and cosnuming them

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ const io = new Server(httpServer, { cors:'http://localhost:3000' });
 
 io.on("connection", (socket) => {
   console.log("server connected")
+  // emit to other socket connection except for the sender else there will be double times whatever drawn
+  socket.on('beginPath',(arg) =>{
+    socket.broadcast.emit('beginPath',arg)
+  })
+  socket.on('drawLine',(arg) =>{
+    socket.broadcast.emit('drawLine',arg)
+  })
+  socket.on('changeConfig',(arg) =>{
+    socket.broadcast.emit('changeConfig',arg)
+  })
 });
 
 httpServer.listen(5000);
